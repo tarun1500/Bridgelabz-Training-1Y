05aecc7e5c56d64e50c5417a7f714a9b9798199d
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-01-15 23:20:16 +0530

    seventeenth commit

---
+++ basic-programming/HarrysAge.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class HarrysAge {
+    public static void main(String[] args) {
+        int birth_year=2000;
+        int current_year=2025;
+        int age=current_year-birth_year;
+        System.out.println("Harry's age in 2025 is:  "+age);
+    }
+}
