712e6b238834dc0947a974e5aaca9b23aa456d3c
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-01-15 23:18:18 +0530

    sixteenth commit

---
+++ basic-programming/HandShakeCal.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class HandShakeCal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = input.nextInt();
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        System.out.println("The maximum number of possible handshakes among " + numberOfStudents + " students is " + handshakes);
+
+    }
+}
+
+
