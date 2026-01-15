76f40d297b43ac8c1251780675fe7cbed8d135db
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-01-15 23:29:31 +0530

    23th commit

---
+++ basic-programming/SamMarks.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class SamMarks {
+    public static void main(String[] args) {
+    double P=95, C=96, M=94;
+    double avg=((P+C+M)/300)*100;
+    System.out.println("Sam’s average mark in PCM is: "+avg+"%");
+    }
+}
