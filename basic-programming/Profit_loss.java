9f05b59e72e21f11ed4567cf0d69a93c2ea3113a
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-01-15 23:27:33 +0530

    22th commit

---
+++ basic-programming/Profit_loss.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Profit_loss {
+    public static void main(String[] args) {
+    float cp=129, sp=191;
+    float pro=sp-cp;
+    float per=sp / cp * 100;
+        System.out.println("The Cost Price is INR "+ cp +" and Selling Price is INR "+sp);
+        System.out.print("The Profit is INR "+pro+" and the Profit Percentage is "+per);
+    }
+}
