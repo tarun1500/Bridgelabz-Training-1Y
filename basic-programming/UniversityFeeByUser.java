9072dc64fb5adcd73889006209a4461482cf5421
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-01-15 23:33:41 +0530

    25th commit

---
+++ basic-programming/UniversityFeeByUser.java
@@ -0,0 +1,18 @@
+package Practice;
+import java.util.Scanner;
+public class UniversityFeeByUser {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the student fee (INR): ");
+        double fee = input.nextDouble();
+        System.out.print("Enter the discount percent: ");
+        double discountPercent = input.nextDouble();
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+
+        System.out.println("The discount amount is INR " + discount +
+                " and final discounted fee is INR " + finalFee);
+
+
+    }
+}
