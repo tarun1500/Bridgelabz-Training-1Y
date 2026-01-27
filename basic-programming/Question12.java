e7d598e812751aee5d05c0b28f1f74b2bc429577
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-01-27 22:23:16 +0530

    Q 12

---
+++ basic-programming/Question12.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Question12{
+    public static void main(String... args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        if (n <= 0) {
+            System.out.println("Not a natural number");
+        } else {
+            int sumWhile = 0;
+            int i = 1;
+
+            while (i <= n) {
+                sumWhile = sumWhile + i;
+                i++;
+            }
+            int sumFormula = n * (n + 1) / 2;
+
+            System.out.println("Sum using while loop: " + sumWhile);
+            System.out.println("Sum using formula: " + sumFormula);
+
+            if (sumWhile == sumFormula) {
+                System.out.println("Both results are correct and equal");
+            } else {
+                System.out.println("Results are NOT equal");
+            }
+        }
+
+        sc.close();
+    }
+}
