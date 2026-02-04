ac56321daeb62d6c75eb370bd529cd332880cf30
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-02-04 23:19:49 +0530

    Student Grade

---
+++ src/com/gla/arrays/.idea/StudentGrades.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class StudentGrades {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        double[] percentage = new double[n];
+        char[] grade = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            int phy = sc.nextInt();
+            int chem = sc.nextInt();
+            int math = sc.nextInt();
+
+            percentage[i] = (phy + chem + math) / 3.0;
+
+            if (percentage[i] >= 90) grade[i] = 'A';
+            else if (percentage[i] >= 75) grade[i] = 'B';
+            else if (percentage[i] >= 60) grade[i] = 'C';
+            else grade[i] = 'D';
+        }
+
+        for (int i = 0; i < n; i++)
+            System.out.println("Percentage: " + percentage[i] + " Grade: " + grade[i]);
+
+        sc.close();
+    }
+}
