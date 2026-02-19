1bb07f45b877209d45757af768ae61bba9f49b63
tarun1500 <tarunkumarsingh8650@gmail.com>
2026-02-19 23:53:12 +0530

    First Commit

---
+++ src/com/gla/Person1.java
@@ -0,0 +1,35 @@
+package com.gla;
+
+interface Worker{
+    void performDuties();
+}
+class Person1{
+    String name;
+    int id;
+    Person1(String name,int id){
+        this.name=name;
+        this.id=id;
+    }
+}
+class Chef extends Person implements Worker{
+    Chef(String name,int id){
+        super(name,id);
+    }
+    public void performDuties(){
+        System.out.println("Cooking");
+    }
+}
+class Waiter extends Person implements Worker{
+    Waiter(String name,int id){
+        super(name,id);
+    }
+    public void performDuties(){
+        System.out.println("Serving");
+    }
+}
+class Main9{
+    public static void main(String[]args){
+        Worker w=new Chef("A",1);
+        w.performDuties();
+    }
+}
---
+++ src/com/gla/Vehicle1.java
@@ -0,0 +1,35 @@
+package com.gla;
+
+interface Refuelable{
+    void refuel();
+}
+class Vehicle1{
+    int maxSpeed;
+    String model;
+    Vehicle1(int maxSpeed,String model){
+        this.maxSpeed=maxSpeed;
+        this.model=model;
+    }
+}
+class ElectricVehicle extends Vehicle{
+    ElectricVehicle(int maxSpeed,String model){
+        super(maxSpeed,model);
+    }
+    void charge(){
+        System.out.println("Charging");
+    }
+}
+class PetrolVehicle extends Vehicle implements Refuelable{
+    PetrolVehicle(int maxSpeed,String model){
+        super(maxSpeed,model);
+    }
+    public void refuel(){
+        System.out.println("Refueling");
+    }
+}
+class Main11{
+    public static void main(String[]args){
+        Refuelable p=new PetrolVehicle(200,"X");
+        p.refuel();
+    }
+}
